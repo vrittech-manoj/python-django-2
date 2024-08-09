d1812bf5dbd6105b8212a32f28d8aa0ce4847071
vrittech-manoj <manoj@vrittechnologies.com>
2024-08-09 16:57:54 +0545

    assignment as calculator

--- 12_assignment.py
+++ 12_assignment.py
@@ -1,2 +1,21 @@
 #WAP to take user input email and validate wether it is correct email or not
 #WAP to take user input mark percentage and find their division.
+
+
+mark_percentage = input("Enter your mark percentage#>>>. ")
+mark_percentage = int(mark_percentage)
+if mark_percentage<30:
+    print("fail")
+elif mark_percentage>30 and mark_percentage<40:
+    print("third division")
+elif mark_percentage>40 and mark_percentage<60:
+    print("second division")
+elif mark_percentage>60 and mark_percentage<80:
+    print("first divi.")
+else:
+    print("distinction")
+
+
+
+
+
---
+++ 13_assignment.py
@@ -0,0 +1,8 @@
+#WAP to make a calculator.
+
+#algorithm of calculator
+# first-step: ask first number from user 
+# second-step: ask operator from user
+# third-step: ask second number from user
+# fourth-step: perform operation based on operator
+
