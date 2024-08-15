ad7f31948d2b135f6b7b138c6ea3abd1fababc85
vrittech-manoj <manoj@vrittechnologies.com>
2024-08-15 17:24:31 +0545

    list

---
+++ 26_list.py
@@ -0,0 +1,44 @@
+name_list = ["ram","hari",1,5,9]
+
+# print(name_list[0])
+
+# print(name_list[-2])
+
+# print(name_list[start:end:step])
+
+# print(name_list[5::-2])
+
+#add element in list
+name_list.append("kathmandu")
+name_list.append("biratnagar")
+
+
+#insert item in list
+name_list.insert(2,"bhaktapur")
+
+
+#pop item from list by positioin
+name_list.pop(0)
+
+
+#remove item from list
+name_list.remove("hari")
+
+
+#find length of list
+print(len(name_list))
+
+#replace item from element
+name_list[0] = "india"
+print(name_list)
+
+
+#Accessing item using loop
+
+
+
+
+#class work find highes digit from list [1,5,9,6,89,1203,4,23]
+
+
+
---
+++ 27_list_loop.py
@@ -0,0 +1,14 @@
+subject_name = ["math","computer","english","nepali","science"]
+
+#Access list data using loop
+# for subject in subject_name:
+#     print(subject,end="     ")
+
+
+#access list item using while loop
+
+i=0
+while i<len(subject_name):
+    print(subject_name[i],end="     ")
+    # i+=1
+    i = i+1
