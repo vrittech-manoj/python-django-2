345e4ec3d2c8e422f76c25ead1944aa467256a9d
vrittech-manoj <manoj@vrittechnologies.com>
2024-08-20 18:06:20 +0545

    prime composite

---
+++ catchup_class/condition_1.py
@@ -0,0 +1,50 @@
+# #types of condition 
+# 1. if  
+# 2. if.....elif.....else(nested condition)   
+# 3. if...else
+    
+
+# #if
+# yadi tapaiko age 18 vanada greater xa vane :
+#      you can vote 
+
+# age = input("Enter your age?")
+# age = int(age)
+
+# if age>18:
+#     print("you can vote")
+# else:
+#     print("you can't vote")
+
+
+#wap to take user input mark percentage and print their division
+        # <30 :- fail 
+        # >30 ra <40 :- third divisison
+        # >40 ra <60 :- second division
+        # >60 ra <80:- first division 
+        # >80 :- distinction 
+
+mark = input("Enter your mark percentage?")
+mark = int(mark)
+if mark<30:
+    print("fail")
+elif True:
+    print("third division")
+
+
+#WAP to take use input tempreature and 
+    
+    "today is very cold" 
+    "today is normal"
+    "today is very hot"
+#WAP to take length and breadth of your house and calculate area of rectangle
+#WAP to cp price,sp price and calculate profit and loss
+#WAP to take user input number and print that number is odd or even.
+#WAP to take user input number and print that number is prime or composite.
+
+
+
+
+
+
+
---
+++ catchup_class/user_input.py
@@ -0,0 +1,3 @@
+age = input("Enter your age#>>>")
+print("your age is:",age)
+print(type(age))
