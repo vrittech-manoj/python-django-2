307d2c55bbde4764322a20d5a178f3e6090fa962
vrittech-manoj <manoj@vrittechnologies.com>
2024-08-26 17:13:16 +0545

    set intersection

--- 38_set_union.py
+++ 38_set_union.py
@@ -2,4 +2,5 @@ A  = {1,5,7}
 B = {2,4,6,7}
 
 a_union_b = A.union(B)
-print(a_union_b)
+a_union_b_without_function = A | B
+print(a_union_b_without_function)
---
+++ 39_set_intersection.py
@@ -0,0 +1,7 @@
+a =  {"ram","hari","sita"}
+b =  {"sita","ram","laxman"}
+a_intersection_b = a.intersection(b)
+print(a_intersection_b)  # Output: {'ram', 'sita'}
+
+a_intersection_b = a & b 
+print(a_intersection_b)  # Output: {'ram', 'sita'}
