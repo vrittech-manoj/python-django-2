b69e014812273cec9e15679315647ffba4a8af03
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-09 17:03:11 +0545

    threading

---
+++ oops/64_threading.py
@@ -0,0 +1,40 @@
+import time 
+from datetime import datetime
+
+import threading
+
+
+def task1():
+    print("Task 1 started")
+    time.sleep(2)
+
+def task2():
+    print("Task 2 started")
+    time.sleep(2)
+
+def task3():
+    print("Task 3 started")
+    time.sleep(2)
+
+t1 = datetime.now()
+print("****************with threading********************")
+
+task_1 = threading.Thread(target=task1)
+task_2 = threading.Thread(target=task2)
+task_3 = threading.Thread(target=task3)
+
+task_1.start()
+task_2.start()
+task_3.start()
+
+
+t2 = datetime.now()
+
+# Calculate execution time
+execution_time = t2 - t1
+
+total_seconds = execution_time.total_seconds()
+print(f"Execution time: {total_seconds} seconds")
+
+
+
---
+++ oops/64_without_threading.py
@@ -0,0 +1,35 @@
+import time 
+from datetime import datetime
+
+
+def task1():
+    print("Task 1 started")
+    time.sleep(2)
+
+def task2():
+    print("Task 2 started")
+    time.sleep(2)
+
+def task3():
+    print("Task 3 started")
+    time.sleep(2)
+
+t1 = datetime.now()
+
+print("****************without threading********************")
+
+task1()
+task2()
+task3()
+
+
+t2 = datetime.now()
+
+# Calculate execution time
+execution_time = t2 - t1
+
+total_seconds = execution_time.total_seconds()
+print(f"Execution time: {total_seconds} seconds")
+
+
+
