b25893ff707fd35a8aac210d43eb5c354c2b365a
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-09 17:23:50 +0545

    downloader

---
+++ oops/downloader/65_downloader.py
@@ -0,0 +1,4 @@
+url1 =  "https://images.pexels.com/photos/813011/pexels-photo-813011.jpeg"
+url2 = "https://images.pexels.com/photos/945177/pexels-photo-945177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+url3  = "https://images.pexels.com/photos/27694923/pexels-photo-27694923/free-photo-of-an-elephant-walking-through-a-grassy-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+url4 = "https://images.pexels.com/photos/28203063/pexels-photo-28203063/free-photo-of-a-person-is-standing-on-the-edge-of-the-water.jpeg?"
